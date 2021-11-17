9a5e0e25c0be251819f73b83759007ad0329da73
sudhirbarik2 <sudhir.barik981@gmail.com>
2021-11-17 14:45:54 +0530

    File upload UI+WS

--- invoice_MS/routes/home-routes.js
+++ invoice_MS/routes/home-routes.js
@@ -35,4 +35,20 @@ router.get('/getDocsById/:userId',(req,res)=>{
     })
     
 })
+router.post('/upload', (req, res) => {
+    if (req.files === null) {
+      return res.status(400).json({ msg: 'No file uploaded' });
+    }
+  
+    const file = req.files.file;
+  
+    file.mv(`${__dirname}/uploadDocs/${file.name}`, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+  
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
+  });
 module.exports = router;
