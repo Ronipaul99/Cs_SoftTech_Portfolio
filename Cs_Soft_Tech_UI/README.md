2ad4423dc6e716f78743323f5740b40edb472724
Ronipaul99 <ronipaul9972@gmail.com>
2021-12-04 19:22:20 +0530

    cs soft website

--- Cs_Soft_Tech_UI/README.md
+++
@@ -1,1 +0,0 @@
-# starter-pack-with-react-and-webpack
---
+++ cs-soft-website/src/pages/sarvice/service-details/DetailsBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DetailsBanner(props) {
+    return (
+        <section className="banner inner-banner">
+            <div className="container">
+                <div className="banner-content">
+                <h2>Services Details</h2>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default DetailsBanner;
